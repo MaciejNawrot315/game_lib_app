9ae5247f08ccfb19a78174500d9ec6b80b7d2d37
skygate <skygate@Katarzynas-MacBook-Pro.local>
2022-07-18 12:30:01 +0200

    loading indicators for home page tiles

--- lib/home_page/home_page_game_tile.dart
+++ lib/home_page/home_page_game_tile.dart
@@ -34,12 +34,13 @@ class _HomePageGameTileState extends State<HomePageGameTile> {
               if (snapshot.hasData) {
                 Future<String> cover =
                     widget.resourceManager.getCoverPic(widget.index);
-                return FutureBuilder<String>(
-                    future: cover,
-                    builder: ((context, snapshot2) {
-                      if (snapshot2.hasData) {
-                        return Stack(children: [
-                          GestureDetector(
+                return Stack(
+                  children: [
+                    FutureBuilder<String>(
+                      future: cover,
+                      builder: ((context, snapshot2) {
+                        if (snapshot2.hasData) {
+                          return GestureDetector(
                             child: Hero(
                               tag: "cover${widget.index}",
                               child: Image.network(snapshot2.data!,
@@ -57,80 +58,95 @@ class _HomePageGameTileState extends State<HomePageGameTile> {
                                 ),
                               );
                             },
+                          );
+                        } else if (snapshot2.hasError) {
+                          return Center(
+                            child: Column(
+                              children: [
+                                const Icon(Icons.error_outline,
+                                    color: Colors.red),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 16),
+                                  child: Text('Error: ${snapshot.error}'),
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                        return const Center(
+                          child: CircularProgressIndicator(
+                            color: Colors.green,
                           ),
-                          Positioned(
-                            bottom: 0,
-                            left: 0,
-                            height: 30,
-                            child: SizedBox(
-                                width: constraints.maxWidth,
-                                height: 30,
-                                child: Container(
-                                    decoration: const BoxDecoration(
-                                        gradient: LinearGradient(
-                                            begin: Alignment(0, -0.7),
-                                            end: Alignment(0, 0.9),
-                                            colors: [
-                                      Color.fromARGB(0, 0, 0, 0),
-                                      Colors.black
-                                    ])))),
-                          ),
-                          Positioned(
-                              bottom: 0,
-                              child: SizedBox(
-                                width: constraints.maxWidth,
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 7.0),
-                                  child: DefaultTextStyle(
-                                    style: TextStyle(color: Colors.white),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceAround,
-                                      children: [
-                                        Flexible(
-                                          child: Tooltip(
-                                            message: snapshot.data!.name,
-                                            waitDuration:
-                                                const Duration(seconds: 1),
-                                            showDuration: const Duration(
-                                                milliseconds: 100),
-                                            preferBelow: false,
-                                            decoration: BoxDecoration(
-                                              color: Colors.black,
-                                              borderRadius:
-                                                  BorderRadius.circular(6),
-                                            ),
-                                            child: Text(
-                                              snapshot.data!.name!,
-                                              overflow: TextOverflow.ellipsis,
-                                            ),
-                                          ),
-                                        ),
-                                        Row(
-                                          children: [
-                                            Transform.scale(
-                                              scale: 0.75,
-                                              child: const Icon(
-                                                Icons.star,
-                                                color: Colors.yellow,
-                                              ),
-                                            ),
-                                            Text((snapshot.data!.rating! / 20.0)
-                                                .toStringAsPrecision(3))
-                                          ],
-                                        )
-                                      ],
+                        );
+                      }),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      height: 30,
+                      child: SizedBox(
+                          width: constraints.maxWidth,
+                          height: 30,
+                          child: Container(
+                              decoration: const BoxDecoration(
+                                  gradient: LinearGradient(
+                                      begin: Alignment(0, -0.7),
+                                      end: Alignment(0, 0.9),
+                                      colors: [
+                                Color.fromARGB(0, 0, 0, 0),
+                                Colors.black
+                              ])))),
+                    ),
+                    Positioned(
+                        bottom: 0,
+                        child: SizedBox(
+                          width: constraints.maxWidth,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 7.0),
+                            child: DefaultTextStyle(
+                              style: TextStyle(color: Colors.white),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Flexible(
+                                    child: Tooltip(
+                                      message: snapshot.data!.name,
+                                      waitDuration: const Duration(seconds: 1),
+                                      showDuration:
+                                          const Duration(milliseconds: 100),
+                                      preferBelow: false,
+                                      decoration: BoxDecoration(
+                                        color: Colors.black,
+                                        borderRadius: BorderRadius.circular(6),
+                                      ),
+                                      child: Text(
+                                        snapshot.data!.name!,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ))
-                        ]);
-                      }
-                      return Container(
-                        height: 154,
-                      );
-                    }));
+                                  Row(
+                                    children: [
+                                      Transform.scale(
+                                        scale: 0.75,
+                                        child: const Icon(
+                                          Icons.star,
+                                          color: Colors.yellow,
+                                        ),
+                                      ),
+                                      Text((snapshot.data!.rating! / 20.0)
+                                          .toStringAsPrecision(3))
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ))
+                  ],
+                );
               } else if (snapshot.hasError) {
                 return Center(
                   child: Column(
@@ -144,12 +160,8 @@ class _HomePageGameTileState extends State<HomePageGameTile> {
                   ),
                 );
               }
-              return Column(
-                children: [
-                  SizedBox(
-                    height: constraints.maxWidth,
-                  ),
-                ],
+              return const Center(
+                child: CircularProgressIndicator(),
               );
             }),
           );
