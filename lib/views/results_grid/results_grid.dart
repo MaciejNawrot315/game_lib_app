51f3ddfdeb905f5a5246b0d3609647b91a640fbe
skygate <maciejowsky315@gmail.com>
2022-07-28 14:49:30 +0200

    loader fixed, proper text displayed

--- lib/views/results_grid/results_grid.dart
+++ lib/views/results_grid/results_grid.dart
@@ -3,6 +3,7 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 import 'package:game_lib_app/models/game/game.dart';
 import 'package:game_lib_app/repositories/igdb_repository.dart';
+import 'package:get/get.dart';
 
 import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
 
@@ -22,6 +23,7 @@ class ResultsGrid extends StatefulWidget {
 class _ResultsGridState extends State<ResultsGrid> {
   bool gamesLoading = false;
   int count = 0;
+  bool isEnd = false;
   List<Game> loadedGames = [];
   Future<void> loadMoreGames() async {
     if (mounted) {
@@ -29,14 +31,17 @@ class _ResultsGridState extends State<ResultsGrid> {
         gamesLoading = true;
       });
     }
-
+    int prevLength = loadedGames.length;
     loadedGames +=
         await IgdbRepository.fetchGamePosters(widget.whereFilters, count);
     count = loadedGames.length;
+    if (count == prevLength) {
+      isEnd = true;
+    } else {
+      gamesLoading = false;
+    }
     if (mounted) {
-      setState(() {
-        gamesLoading = false;
-      });
+      setState(() {});
     }
   }
 
@@ -59,6 +64,13 @@ class _ResultsGridState extends State<ResultsGrid> {
             itemCount: count + 1,
             itemBuilder: (BuildContext context, index) {
               if (index >= count) {
+                if (isEnd) {
+                  return Text(
+                    'no_more_results'.tr,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.grey[300]),
+                  );
+                }
                 return const Center(
                   child: CircularProgressIndicator(),
                 );
